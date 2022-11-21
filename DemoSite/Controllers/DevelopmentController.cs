f1c3b7f0ad1c9d129d7efcedf5872e95c94cf560
Ghislaine Laios <laios54@proton.me>
2022-11-21 20:27:54 +0800

    Update development controller.

---
+++ DemoSite/Controllers/DevelopmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace DemoSite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevelopmentController : ControllerBase
+    {
+        [HttpPost("UploadFile")]
+        public object UploadFile(IFormFile file)
+        {
+            return file;
+        }
+
+        [HttpPost("Raw")]
+        public async Task<object> EchoRaw()
+        {
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
